93ec709af75115963d684e448116a984787f367f
Melimet <erkka.rahikainen@gmail.com>
2022-10-19 16:53:21 +0300

    1.11

--- week1/unicafe/src/App.tsx
+++ week1/unicafe/src/App.tsx
@@ -1,68 +1,91 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 type values = {
-  good: number,
-  neutral: number,
-  bad: number
-}
-function Total({good, neutral, bad}: values){
-  return <p>Total: {good+neutral+bad}</p>
-}
-
-function Average({good, neutral, bad}: values){
-  return <p>Average: {(good - bad)/(good+neutral+bad)}</p>
-}
-
-function Positive({good, neutral, bad}: values){
-  return <p>Positive: {good / (good+neutral+bad)}%</p>
-}
-
-function StatisticLine({text, value}: {text:string, value: number}){
-  return <p>{text} {value}</p>
-}
-
-function Statistics({good, neutral, bad}: values){
-  
-  if (!good && !neutral && !bad) 
+  good: number;
+  neutral: number;
+  bad: number;
+};
+function Total({ good, neutral, bad }: values) {
   return (
-  <>
-    <h2>statistics</h2> 
-    <p>No feedback given</p>
-  </>
+    <tr>
+      <td>Total</td>
+      <td>{good + neutral + bad}</td>
+    </tr>
   )
-
-  return <>
-      <h2>statistics</h2>
-
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-
-      <Total good={good} neutral={neutral} bad={bad}/>
-      <Average good={good} neutral={neutral} bad={bad}/> 
-      <Positive good={good} neutral={neutral} bad={bad}/>
-    </>
 }
 
-function Button({name}: {name:string}){
-  return <button>{name}</button>
+function Average({ good, neutral, bad }: values) {
+  return (
+    <tr>
+      <td>Average:</td>
+      <td>{(good - bad) / (good + neutral + bad)}</td>
+    </tr>
+  );
+}
+
+function Positive({ good, neutral, bad }: values) {
+  return (
+    <tr>
+      <td>Positive:</td>
+      <td>{good / (good + neutral + bad)}%</td>
+    </tr>
+  );
+}
+
+function StatisticLine({ text, value }: { text: string; value: number }) {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+}
+
+function Statistics({ good, neutral, bad }: values) {
+  if (!good && !neutral && !bad)
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+
+  return (
+    <>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+
+          <Total good={good} neutral={neutral} bad={bad} />
+          <Average good={good} neutral={neutral} bad={bad} />
+          <Positive good={good} neutral={neutral} bad={bad} />
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+function Button({ name }: { name: string }) {
+  return <button>{name}</button>;
 }
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(1);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button name={"good"}/>
-      <Button name={"neutral"}/>
-      <Button name={"bad"}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-
+      <Button name={"good"} />
+      <Button name={"neutral"} />
+      <Button name={"bad"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

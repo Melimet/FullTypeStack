5fbd795a9a637a0792981543808f4baa25d137e7
Melimet <erkka.rahikainen@gmail.com>
2022-10-20 13:53:10 +0300

    2.5

--- week1/kurssitiedot/src/App.tsx
+++ week1/kurssitiedot/src/App.tsx
@@ -1,40 +1,4 @@
-function Header({ course }: {course: string }) {
-  return <h1>{course}</h1>
-}
-
-type PartType = {
-  name: string,
-  exercises: number,
-  id: number,
-}
-
-function Part({ name, exercises, id}: PartType) {
-  return <p>{name}, exercises: {exercises}</p>
-}
-
-function Content({ parts }: {parts: PartType[]}){
-  return (
-    <div>
-      {parts.map((part) => <Part key={part.name} name={part.name} exercises={part.exercises} id={part.id} />)}
-    </div>
-  )
-}
-
-function Total({ parts }: {parts: PartType[]}){
-  return <p>
-  Total exercises: {parts.reduce((sum, current) => sum + current.exercises, 0)}
-  </p>
-}
-
-function Course({ course: { name, id, parts } }: { course: { name: string, id: number, parts: PartType[] } }) {
-  return (
-    <div>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total parts={parts}/>
-    </div>
-  ) 
-}
+import { Course } from './Components/Course'
 
 function App() {
   

8ae7cfb7c41ce93ed3484d2e64036043047b1cce
Melimet <erkka.rahikainen@gmail.com>
2022-11-02 16:35:54 +0200

    5.6, 5.7

--- week5/bloglist-frontend/src/components/Blog.tsx
+++ week5/bloglist-frontend/src/components/Blog.tsx
@@ -1,14 +1,29 @@
 import { BlogType } from "../types"
 import "../index.css"
+import { useState } from "react"
 interface BlogProps {
   blog: BlogType
 }
 
 function Blog({ blog }: BlogProps) {
+
+  const [visible, setVisible] = useState(false)
+
+  function toggleVisibility() {
+    setVisible(!visible)
+  }
+
   return (
     <div className="blog">
       <h3>{blog.title}</h3>
-      <p>{blog.author}</p>
+      {!visible &&
+        <button onClick={toggleVisibility}>show</button>}
+      {visible &&<>
+        <p>By: {blog.author}</p>
+        <p>{blog.url}</p>
+        <button onClick={toggleVisibility}>hide</button>
+        </>
+      }
     </div>
   )
 }

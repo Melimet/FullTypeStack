fae8eef93704c414ce51340d1682c56737e33c5d
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2022-11-16 15:18:51 +0200

    Add notification reducer to store

--- week5/bloglist-frontend/src/reducers/notificationReducer.ts
+++ week5/bloglist-frontend/src/reducers/notificationReducer.ts
@@ -1,7 +1,30 @@
 import { createSlice, Dispatch } from "@reduxjs/toolkit"
 
 const initialState = ""
+let timer: ReturnType<typeof setTimeout>
+
 
 const notificationSlice = createSlice({
-  
+  name: "notifications",
+  initialState,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    }
+  }
 })
+
+export function createNotification(message: string, time: number) {
+  return async (dispatch: Dispatch) => {
+    dispatch(setNotification(message))
+
+    if (timer) clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      dispatch(setNotification(""))
+    }, time*1000)
+  }
+}
+
+export const { setNotification } = notificationSlice.actions
+export default notificationSlice.reducer
--- week5/bloglist-frontend/src/store.ts
+++ week5/bloglist-frontend/src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import notificationReducer from "./reducers/notificationReducer";
+
 
 const store = configureStore({
   reducer: {
-
+    notifications: notificationReducer
   }
 })
 

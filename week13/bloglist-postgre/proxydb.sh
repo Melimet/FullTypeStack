27a9554d521cc2451d935283990ea551b5df948f
erkka <erkka.rahikainen@gmail.com>
2022-11-25 15:43:19 +0200

    13.3

---
+++ week13/bloglist-postgre/proxydb.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+flyctl proxy 5432 -a lingering-haze-6378-db
--- week13/bloglist-postgre/src/app.ts
+++ week13/bloglist-postgre/src/app.ts
@@ -1,11 +1,33 @@
-import express from 'express'
+import express from "express"
+import { QueryTypes, Sequelize } from "sequelize"
+require("dotenv").config()
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.get("/", (_req, res) => {
+  res.send("Hello World!")
+})
+if (!process.env.DATABASE_URL) {
+  console.log("No database url provided")
+  process.exit(1)
+}
 
-export {app}
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log("Connection has been established successfully.")
+    const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT})
+    console.log(blogs)
+    sequelize.close()
+  } catch (error) {
+    console.error("Unable to connect to the database:", error)
+  }
+}
+
+main()
+
+export { app }
